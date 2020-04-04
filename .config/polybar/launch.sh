d9253f1241b335225d6938037c6e36dce1c43954
jmnguye <nguyen.jean.michel@gmail.com>
2020-04-05 00:18:23 +0200

    remove the hdmi lvds settins, and used the connected script to retrieve the connected screen

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,26 +1,8 @@
 #!/usr/bin/env bash
 
-if [ "$1" == "" ] ; then
-	echo "Missing parameter : hdmi , lvds, dual"
-else
-	killall -q polybar
-	
-	case $1 in
-	hdmi)
-		outputValues=HDMI-1
-		;;
-	lvds)
-		outputValues=LVDS-1
-		;;
-	dual)
-		outputValues="HDMI-1 LVDS-1"
-		;;
-	esac
+for m in $(xrandr | grep " connected" | awk '{print $1}' | xargs); do
+	echo "---" | tee -a /tmp/polybar${m}.log /tmp/polybar${m}.log
+	MONITOR=$m polybar --reload mybar >> /tmp/polybar${m}.log 2>&1 &
+done
 
-	for m in $(echo ${outputValues}); do
-		echo "---" | tee -a /tmp/polybar${m}.log /tmp/polybar${m}.log
-		MONITOR=$m polybar --reload mybar >> /tmp/polybar${m}.log 2>&1 &
-	done
-
-	echo "Bars launched..."
-fi
+echo "Bars launched..."
